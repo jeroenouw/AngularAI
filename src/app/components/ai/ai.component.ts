ce442a8793c3dc132474ffe902dc6b57f181c4c1
Jeroenouw <jeroenouw@hotmail.com>
2017-10-29 10:13:05 +0100

    AI ts html update and minor changes

--- src/app/components/ai/ai.component.ts
+++ src/app/components/ai/ai.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/scan';
+
+import { AiService } from '../../service/ai.service';
+import { Message } from '../../model/message';
 
 @Component({
   selector: 'app-ai',
   templateUrl: './ai.component.html',
   styleUrls: ['./ai.component.scss']
 })
-export class AiComponent {
-  title = 'app works!';
+export class AiComponent implements OnInit {
+    allMessages: Observable<Message[]>;
+    formInput: string;
+
+    constructor(public ai: AiService) { 
+    }
+
+    ngOnInit() {
+      this.allMessages = this.ai.conversation.asObservable()
+          .scan((acc, val) => acc.concat(val) );
+    }
+
+    sendMessageToBot() {
+      this.ai.converse(this.formInput);
+      this.formInput = '';
+    }
 }
+  
